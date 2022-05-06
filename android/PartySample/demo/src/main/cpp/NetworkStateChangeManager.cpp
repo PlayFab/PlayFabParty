87f5b5b52a72e261e452238a97bd91d874fc2957
Scott Munro <scmunro@microsoft.com>
2022-05-06 16:33:57 -0700

    updating the android sample cpp code with latest changes from private repo

--- android/PartySample/demo/src/main/cpp/NetworkStateChangeManager.cpp
+++ android/PartySample/demo/src/main/cpp/NetworkStateChangeManager.cpp
@@ -5,31 +5,40 @@ NetworkStateChangeManager::NetworkStateChangeManager()
 }
 
 void
-NetworkStateChangeManager::onPlayerLeft(
-    PartyString playerId
+NetworkStateChangeManager::ProcessStatusMessage(
+    const std::string& system,
+    const std::string& message
     )
 {
-    OnChatControlDestroyed(GetUserName(playerId));
-    m_userMap.erase(playerId);
+    // Route status messages as fake endpoint messages so they show up in the chat window.
+    OnEndpointMessageReceived(system.c_str(), message.c_str());
 }
 
 void
-NetworkStateChangeManager::ProcessEndpointMessage(
-    std::string& sender,
-    std::string& message
+NetworkStateChangeManager::ProcessPlayerJoined(
+    const std::string& playerEntityId,
+    const std::string& displayName
     )
 {
 
-    //OnEndpointMessageReceived(GetUserName(sender).c_str(), message.c_str());
-    m_userMap.emplace(sender, message);
-    OnChatControlCreated(GetUserName(sender).c_str());
+    m_userMap.emplace(playerEntityId, displayName);
+    OnChatControlCreated(GetUserName(playerEntityId).c_str());
+}
+
+void
+NetworkStateChangeManager::ProcessPlayerLeft(
+    const std::string& playerEntityId
+    )
+{
+    OnChatControlDestroyed(GetUserName(playerEntityId));
+    m_userMap.erase(playerEntityId);
 }
 
 // Called when a text chat message is sent to the chat control.
 void
 NetworkStateChangeManager::ProcessTextMessage(
-    std::string& sender,
-    std::string& message
+    const std::string& sender,
+    const std::string& message
     )
 {
     OnEndpointMessageReceived(GetUserName(sender).c_str(), message.c_str());
@@ -38,11 +47,31 @@ NetworkStateChangeManager::ProcessTextMessage(
 // Called when a voice transcription is sent to the chat control.
 void
 NetworkStateChangeManager::ProcessVoiceMessage(
-    std::string& sender,
-    std::string& message
+    const std::string& sender,
+    const std::string& message
     )
 {
     OnVoiceChatTranscriptionReceived(GetUserName(sender).c_str(), message.c_str());
+}
+
+// Called when a local chat indicator changes
+void
+NetworkStateChangeManager::ProcessLocalChatIndicatorChange(
+    const std::string&,
+    Party::PartyLocalChatControlChatIndicator
+    )
+{
+    // noop. Android doesn't currently use this for exposing chat indicator changes.
+}
+
+// Called when a remote chat indicator changes
+void
+NetworkStateChangeManager::ProcessRemoteChatIndicatorChange(
+    const std::string&,
+    Party::PartyChatControlChatIndicator
+    )
+{
+    // noop. Android doesn't currently use this for exposing chat indicator changes.
 }
 
 const
